2a8d715e36653352252e9728874b45566e1a8b94
Thomas Rebele <thomas.rebele@googlemail.com>
2018-12-02 19:26:40 +0100

    adding test driver for nested session

---
+++ test-xephyr.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+# A script to test the grafical part of the dotfiles, using the lightdm display manager.
+# It requires the following packages:
+# - xautomation
+# - xdotool
+# - lightdm
+
+# create temporary user
+### #user=tmp_$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 8 | head -n 1)
+user=tmptr
+pwd=a
+
+sudo /usr/sbin/useradd -p $(openssl passwd -1 $pwd)  $user
+
+# create nested session
+dm-tool add-nested-seat --screen 1000x800
+sleep 2
+
+# login temporary user
+xdotool type tmptr
+xte 'key Tab'
+sleep 1
+xdotool type $pwd
+xte 'key Return'
+
+###### maybe possible with xephyr?
+#### disp=4
+#### Xephyr -ac -screen 800x400 -br -reset -terminate 2> /dev/null :$disp & 
+#### sudo -u $user startx --  :$disp
+
+
+# wait until xephyr closed
+while ps -eo user,comm | grep $(whoami) | grep --quiet Xephyr; do
+	sleep 1
+done
+
+## read -p "print any key to clean up" -n 1 -s
+
+# kill processes of temporary user
+while ps -eo user | grep --quiet $user; do
+	for i in $(ps -eo pid,user | grep $user | awk '{print $1}'); do
+		sudo kill -9 $i
+	done
+	sleep 1
+done
+
+# delete temporary user
+sudo deluser $user
+
