f695b26e3f8cf37ad6fe33ab1bbad996938c94cd
Thomas Rebele <thomas.rebele@googlemail.com>
2019-02-12 23:09:07 +0100

    path shortener

---
+++ scripts/short-pwd.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+
+
+def shorten(path, length):
+    # http://stackoverflow.com/a/1616781/1562506
+    while len(path) > length:
+        dirs = path.split("/");
+
+        # Find the longest directory in the path.
+        max_index  = -1
+        max_length = 3
+
+        for i in range(len(dirs)):
+            if len(dirs[i]) > max_length:
+                max_index  = i
+                max_length = len(dirs[i])
+
+        # Shorten it by one character.
+        if max_index >= 0:
+            dirs[max_index] = dirs[max_index][:max_length-3]
+            path = "/".join(dirs)
+
+        # Didn't find anything to shorten. This is as good as it gets.
+        else:
+            break
+    return path
+
+# adapted from http://askubuntu.com/a/17738/351417
+import os, sys
+#from socket import gethostname
+#hostname = gethostname()
+#username = os.environ['USER']
+if(len(sys.argv) > 1):
+    pwd = sys.argv[1]
+else:
+    pwd = os.getcwd()
+homedir = os.path.expanduser('~')
+pwd = pwd.replace(homedir, '~', 1)
+pwd = shorten(pwd, 30)
+
+print('{}'.format(pwd))
