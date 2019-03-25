473e072c0737c115d257d89de319ee6dec445ddb
Thomas Rebele <thomas.rebele@googlemail.com>
2019-03-25 17:29:37 +0100

    expect template

--- scripts/sys/lightdm-login.sh
+++ scripts/sys/lightdm-login.sh
@@ -4,4 +4,5 @@
 zgrep -e "Error getting user list from org.freedesktop.Accounts" \
 	-e "Stopped target Graphical Interface" \
 	-e "Reached target \(Graphical Interface\|Shutdown\)" \
-	$(/bin/ls /var/log/syslog* | sort -r) | sed 's/^[^:]*://' | sed 's/lightdm.*//'
+	-e "systemd\[1\]: Stopped" \
+	$(/bin/ls /var/log/syslog* | sort -r) | sed 's/^[^:]*://' | sed 's/lightdm.*//' 
