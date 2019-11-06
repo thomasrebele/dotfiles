2f214d8c088a923f1cbe5ba33c9828b32a8a48f7
Thomas Rebele <thomas.rebele@googlemail.com>
2019-11-06 21:53:29 +0100

    fix quoting

--- scripts/pdf/pdfcompress-scan-mono.sh
+++ scripts/pdf/pdfcompress-scan-mono.sh
@@ -5,20 +5,26 @@ if [ $# -lt 2 ]; then
 	exit 1
 fi
 
+echo "input: $1"
 TMP=/tmp/pages-TODO
-INPUT=$(realpath $1)
-OUTPUT=$(realpath $2)
+INPUT="$(realpath "$1")"
+echo "input: $INPUT"
+OUTPUT="$(realpath "$2")"
 
 mkdir -p $TMP
 cd $TMP
+echo "splitting"
 pdfimages "$INPUT" pages
 
+echo "mono"
 for i in ./pages*.ppm; do 
-	convert "$i" -resize 35% -colorspace gray  -quality 50 -density 100 -fill white -fuzz 80% -auto-level -depth 4 +opaque '#000000' "$i.jp2"; 
+	convert "$i" -resize 50% -colorspace gray  -quality 70 -density 100 -fill white -fuzz 80% -auto-level -depth 4 +opaque '#000000' "$i.jp2"; 
 done
 
+echo "convert to pdf"
 convert *.jp2 out.pdf
 
+echo "compress"
 ~/.dotfiles/scripts/pdf/pdfcompress.sh out.pdf "$OUTPUT"
 
 
