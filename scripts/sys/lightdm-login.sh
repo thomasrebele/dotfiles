2f20c2d5824b576e339d9c2213d08cc310179d04
Thomas Rebele <thomas.rebele@googlemail.com>
2020-01-27 05:17:31 +0100

    xmonad horizontal dual monitor float

--- scripts/sys/lightdm-login.sh
+++ scripts/sys/lightdm-login.sh
@@ -5,4 +5,4 @@ zgrep --text -e "Error getting user list from org.freedesktop.Accounts" \
 	-e "Stopped target Graphical Interface" \
 	-e "Reached target \(Graphical Interface\|Shutdown\)" \
 	-e "systemd\[1\]: Stopped" \
-	$(/bin/ls /var/log/syslog* | sort -r) | sed 's/^[^:]*://' | sed 's/lightdm.*//' 
+	$(/bin/ls -rt /var/log/syslog*) | sed 's/^[^:]*://' | sed 's/lightdm.*//' 
