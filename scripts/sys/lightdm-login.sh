31d9317481300f1229fc09d9593a50f73727419b
Thomas Rebele <thomas.rebele@googlemail.com>
2021-07-09 19:01:41 +0200

    vars, broot, xmonad flex resize, lightdm logins

--- scripts/sys/lightdm-login.sh
+++ scripts/sys/lightdm-login.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
+files="$(find /var/log -name 'syslog*' -newermt $(date +%Y-%m-%d -d '28 day ago') 2> /dev/null)"
 
 zgrep --text -e "Error getting user list from org.freedesktop.Accounts" \
 	-e "Stopped target Graphical Interface" \
 	-e "Reached target \(Graphical Interface\|Shutdown\)" \
 	-e "systemd\[1\]: Stopped" \
-	$(/bin/ls -rt /var/log/syslog*) | sed 's/^[^:]*://' | sed 's/lightdm.*//' 
+	$(/bin/ls -rt $files) | sed 's/^[^:]*://' | sed 's/lightdm.*//' 
