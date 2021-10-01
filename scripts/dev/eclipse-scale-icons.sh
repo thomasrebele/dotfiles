c9aa6f4205e3e2d0fa72576321af5c6cda4fccdc
Thomas Rebele <thomas.rebele@googlemail.com>
2021-10-01 12:43:15 +0200

    to 4k screen

--- scripts/dev/eclipse-scale-icons.sh
+++ scripts/dev/eclipse-scale-icons.sh
@@ -4,7 +4,7 @@
 # improved: https://gist.github.com/gene1wood/3b4b37d5a438871b8876
 
 resize_images() {
-    find "$1" -iname "*.png" -o -iname "*.gif" -exec sh -c "expr \$(identify -format \"%h\" {}) \<= 16 > /dev/null" \; -exec convert {} -resize 200% {} \; -printf "%f resized\n"
+	find "$1" \( -iname "*.png" -o -iname "*.gif" \) -exec sh -c "expr \$(identify -format \"%h\" {}) \< 32 > /dev/null" \; -exec convert "{}" -resize 400% {} \; -printf "%f resized\n" | grep '.'
 }
 
 
@@ -18,16 +18,19 @@ do
     unzip -q "$f" -d "$d"
 
     echo "Processing images in $J..."
-    resize_images "$d"
+    if resize_images "$d"; then
 
-    echo "Compressing $J..."
-    mv -f "$f" "$f.`date +%Y%m%d%H%M%S`.bak"
-    pushd "$d" >/dev/null
-    zip -qr "$f" .
-    popd >/dev/null
+        echo "Compressing $J..."
+        #mv -f "$f" "$f.`date +%Y%m%d%H%M%S`.bak"
+        pushd "$d" >/dev/null
+        zip -qr "$f" .
+        popd >/dev/null
+    fi
 
     rm -rf "$d"
 done
 
 echo "Processing all images outside of jars..."
 resize_images "."
+
+
