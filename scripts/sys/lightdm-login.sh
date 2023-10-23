04508d56a6481d6eda9500e9d27a7c6ee147cd8d
Thomas Rebele <thomas.rebele@gmail.com>
2023-10-23 16:15:27 +0200

    upgrade xmonad.hs to 0.17

--- scripts/sys/lightdm-login.sh
+++ scripts/sys/lightdm-login.sh
@@ -5,5 +5,8 @@ files="$(find /var/log -name 'syslog*' -newermt $(date +%Y-%m-%d -d '28 day ago'
 zgrep --text -e "Error getting user list from org.freedesktop.Accounts" \
 	-e "Stopped target Graphical Interface" \
 	-e "Reached target \(Graphical Interface\|Shutdown\)" \
+	-e "Reached target Sleep" \
+	-e "Starting Suspend" \
+	-e "systemd-sleep" \
 	-e "systemd\[1\]: Stopped" \
 	$(/bin/ls -rt $files) | sed 's/^[^:]*://' | sed 's/lightdm.*//' 
